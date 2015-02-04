cbac81d254a845fae585afcc64896b3934a50bd3
Ricardo Gladwell <ricardo@gladwell.me>
2015-02-04 18:09:57 +0000

    Refactored test specification.
    Refactored test specification with utility methods to make calls to
    Dispatch more readable.

--- src/test/scala/me/gladwell/aws/ApiSpec.scala
+++ src/test/scala/me/gladwell/aws/ApiSpec.scala
@@ -17,52 +17,43 @@ object ApiSpec extends Specification with unfiltered.specs2.jetty.Served with Xm
 
   def endpoint = url(s"http://localhost:$port")
 
+  def status(request: Request) = new Http x (request as_str) {
+    case (code, _, _, _) => code
+  }
+
+  def contentType(request: Request) = new Http x (request as_str) {
+    case (_, response, _, _) => response.getHeaders("Content-Type")(0).getValue
+  }
+
+  def html(request: Request) = new Http x (request as_tagsouped)
+
   "The HTTP API" should {
     "return OK response for an index request" in {
-      val status = new Http x (endpoint as_str) {
-        case (code, _, _, _) => code
-      }
-
-      status must_== 200
+      status(endpoint) must_== 200
     }
 
     "return html response for an index request" in {
-      val contentType = new Http x (endpoint as_str) {
-        case (_, response, _, _) => response.getHeaders("Content-Type")(0).getValue
-      }
-
-      contentType must startWith("text/html")
+      contentType(endpoint) must startWith("text/html")
     }
 
     "return a form for an index request" in {
-      val html = new Http x (endpoint as_tagsouped)
-      html must \\("form")
+      html(endpoint) must \\("form")
     }
 
     "return a form with address field for an index request" in {
-      val html = new Http x (endpoint as_tagsouped)
-      html must \\("input", "name" -> "address")
+      html(endpoint) must \\("input", "name" -> "address")
     }
 
     "return OK response for an address lookup" in {
-      val status = new Http x (endpoint / "?address=example.com" as_str) {
-        case (code, _, _, _) => code
-      }
-
-      status must_== 200
+      status(endpoint / "?address=example.com") must_== 200
     }
 
     "return html response for an address lookup" in {
-      val contentType = new Http x (endpoint / "?address=example.com" as_str) {
-        case (_, response, _, _) => response.getHeaders("Content-Type")(0).getValue
-      }
-
-      contentType must startWith("text/html")
+      contentType(endpoint / "?address=example.com") must startWith("text/html")
     }
 
     "return a result for an address lookup" in {
-      val html = new Http x (endpoint / "?address=example.com" as_tagsouped)
-      html must \\("span", "id" -> "is-aws")
+      html(endpoint / "?address=example.com")  must \\("span", "id" -> "is-aws")
     }
   }
 
